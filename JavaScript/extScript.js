e3032c8169f3357a0f5dc5bcd5fb47fabacb992b
Sivaditya Vardhan Borra <sivadityavardhan@gmail.com>
2023-12-09 11:38:36 +0530

    fundamentals_1

---
+++ JavaScript/extScript.js
@@ -0,0 +1,9 @@
+let js = "Jarvis";
+/* console.log(10 + 30 + 15 + 5 + 40);
+console.log("JONAS");
+console.log(js); */
+
+let firstName = "Tony";
+let lastName = "Stark";
+
+console.log(firstName + " " + lastName);
