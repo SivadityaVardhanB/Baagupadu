f136c9931b69310f9d5a874dc193230cfefc00dd
Sivaditya Vardhan Borra <sivadityavardhan@gmail.com>
2023-12-14 12:24:10 +0530

    fundementals1

--- JavaScript/extScript.js
+++ JavaScript/extScript.js
@@ -3,7 +3,22 @@ let js = "Jarvis";
 console.log("JONAS");
 console.log(js); */
 
-let firstName = "Tony";
+let firstName = "Tinku";
 let lastName = "Stark";
 
+const currentYear = 2023;
+const ageTinku = currentYear - 1998;
+const ageStark = currentYear - 2000;
+
 console.log(firstName + " " + lastName);
+console.log(ageTinku, ageStark);
+
+const newName = `I'm ${firstName + " " + lastName}! `;
+console.log(newName);
+
+if (ageTinku >= 18) {
+  console.log("I have a driver's license 🚗");
+} else {
+    const yearsRequired = 18 - ageTinku
+  console.log(`wait for ${yearsRequired} to get driver's license`);
+}
